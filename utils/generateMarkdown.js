59585b87bd2924ef69904a3ea3d6a9eda06e0177
Nathan Long <nrlong13@gmail.com>
2020-03-29 19:25:29 -0400

    updated api call and markdown generation

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,50 @@
-
-
 function generateMarkdown(data) {
   return `
 # ${data.title}
+
+![Github ALl Releases](http://img.shields.io/github/downloads/${data.username}/${data.title}/total)
+
+## Description
+
+${data.description}
+
+
+## Table of Contents
+
+* [Description]{##Description}
+* [Installation]{##Installation}
+* [Useage]{##Useage}
+* [License]{##License}
+* [Contributors]{##Contributors}
+* [Tests]{##Tests}
+* [Questions]{##Questions}
+
+##Installation
+
+${data.install}
+
+##Useage
+
+${data.useage}
+
+##License
+
+![Github license](https://image.shields.io/badge/license-${data.license}-blue.svg)
+
+##Contributors
+
+${data.contributors}
+
+##Tests
+
+${data.testing}
+
+##Questions
+
+${data.questions}
+
+![User's Avatar](${data.avatarURL})
+${data.email}
 
 `;
 }
